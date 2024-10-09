5b381d37ae79ea10729b7c5ee29426141a2490e9
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 17:02:49 -0300

    feat: Add routes for service order management
    - Added POST route `/serviceorder/create` for creating a new service order using `addServiceOrder` method.
    - Added POST route `/serviceorder/update` for updating an existing service order using `updateServiceOrder` method.
    - Added GET route `/serviceorder/list` for listing all service orders using `listServiceOrders` method.

---
+++ src/app/routes/serviceOrderRoute.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { ServiceOrderController } from "../controllers/serviceOrderController";
+
+const router = Router();
+
+router.post(
+  "/serviceorder/create",
+  ServiceOrderController.addServiceOrder.bind(ServiceOrderController)
+);
+
+router.post(
+  "/serviceorder/update",
+  ServiceOrderController.updateServiceOrder.bind(ServiceOrderController)
+);
+
+router.get(
+  "/serviceorder/list",
+  ServiceOrderController.listServiceOrders.bind(ServiceOrderController)
+);
+
+export default router;
