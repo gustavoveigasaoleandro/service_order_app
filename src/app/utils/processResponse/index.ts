3e753102a4d77bfaba3089341659be783dce529a
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 17:01:20 -0300

    feat: Add processResponse utility for authorization validation
    - Created `processResponse` function to process RabbitMQ responses.
    - Validates response structure, ensuring it includes the required `role` and `valid` flag.
    - Returns a structured `ValidResponse` object with user and company data if validation passes.
    - Handles error scenarios by returning an invalid response when conditions are not met.

---
+++ src/app/utils/processResponse/index.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+import { ResponseStructure } from "../../interface/authorization_responseStructure";
+import { ValidResponse } from "../../interface/validResponse";
+type ResponseType = ResponseStructure | { error: string };
+export function processResponse(
+  response: ResponseType,
+  res: Response,
+  requiredRole: string
+): ValidResponse {
+  if ("valid" in response && response.valid && response.role === requiredRole) {
+    return {
+      valid: true,
+      userId: response.userId,
+      companyId: response.companyId,
+    };
+  } else {
+    return { valid: false, userId: null, companyId: null };
+  }
+}
