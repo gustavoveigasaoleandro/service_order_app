bfaf8bc5b2b2753fe167168f2fe4d6de6d2046de
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-11-18 12:29:58 -0300

    feat(server): enhance server configuration with CORS, environment variables, and logging
    - Added `dotenv` for environment variable management, allowing dynamic port configuration via `process.env.PORT`.
    - Introduced middleware to configure and handle CORS headers (`Access-Control-Allow-Origin`, `Access-Control-Allow-Headers`, and `Access-Control-Allow-Methods`).
    - Implemented support for preflight CORS requests by responding to `OPTIONS` method with HTTP 204.
    - Added middleware to log CORS application for debugging purposes.
    - Updated server start message for consistency.
    - Retained existing functionalities, ensuring backward compatibility.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { syncModels } from "./app/models/index.js";
 import routes from "./app/routes/index.js";
 import errorHandler from "./app/middleware/errorHandler.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+// Define a porta pelo ambiente ou usa a porta 3000 como padrão
+const port = process.env.PORT || 3001;
 
-const port = 3001;
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Origin", "*"); // Substitua pelo domínio desejado
+  res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE");
 
+  // Permitir o preflight CORS (método OPTIONS)
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+// Middleware para log de CORS (para debug)
+app.use((req, res, next) => {
+  console.log(`CORS aplicado para ${req.method} ${req.url}`);
+  next();
+});
 //startApp();
 
 routes(app);
